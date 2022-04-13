4733037bf97a6ba8899596b2ecd2def54c644391
hannesbachmann <hannes-bachmann@t-online.de>
2022-04-13 17:59:03 +0200

    display pairplots and heatmaps from items.csv and orders.csv

---
+++ first_view.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+def displaying_csv_pairplots():
+    df = pd.read_csv('items.csv', delimiter='|')
+    cols = ['itemID', 'brand', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
+    sns.pairplot(df[cols], height=2, plot_kws={"s": 3})
+    plt.tight_layout()
+    plt.show()
+
+    df2 = pd.read_csv('orders.csv', delimiter='|', parse_dates=True)
+    first_date = pd.to_datetime(df2['date'][0]).value
+    df2['date'] = df2.apply(lambda row: pd.to_datetime(row['date']).value - first_date, axis=1)
+    cols2 = ['date', 'userID', 'itemID', 'order']
+    sns.pairplot(df2[cols2], height=2.5, plot_kws={"s": 3})
+    plt.tight_layout()
+    plt.show()
+
+
+def correlations():
+    """find linear correlations"""
+
+    df = pd.read_csv('items.csv', delimiter='|')
+    cols = ['itemID', 'brand', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
+    cm = np.corrcoef(df[cols].values.T)
+    sns.set(font_scale=1.5)
+    heat_map1 = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 15},
+                            yticklabels=cols, xticklabels=cols)
+    plt.show()
+
+    df2 = pd.read_csv('orders.csv', delimiter='|', parse_dates=True)
+    first_date = pd.to_datetime(df2['date'][0]).value
+    df2['date'] = df2.apply(lambda row: pd.to_datetime(row['date']).value - first_date, axis=1)
+    cols2 = ['date', 'userID', 'itemID', 'order']
+    cm = np.corrcoef(df2[cols2].values.T)
+    sns.set(font_scale=1.5)
+    heat_map2 = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 15},
+                            yticklabels=cols2, xticklabels=cols2)
+    plt.show()
+    pass
+
+
+if __name__ == '__main__':
+    correlations()
